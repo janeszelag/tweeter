08e6482bf3a62013600c61bb004064bee5043421
Katherine <katherine.szelag@mail.utoronto.ca>
2019-12-17 19:45:39 +0000

    added character count, text becomes red when in the negatives

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+  const maxLength = 140;
+
+  $("#form-text").keydown(function() {
+    let length = $(this).val().length;
+    tweetLength = maxLength-length;
+    let $counter = $(this).siblings("span");
+    $counter.text(tweetLength);
+    
+    if (tweetLength < 0) {
+      $counter.addClass( "red-txt" );
+    }
+  })
+});
